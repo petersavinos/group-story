6bfcc177d8b668d4844dacc4d05130e1a6cdbd0e
Peter Savinos <psavinos@depaul.edu>
2025-04-14 23:34:36 -0500

    Hotfix: Prevent withdrawals with balance of zero and add 1% interest to deposits.

--- BankAccount.java
+++ BankAccount.java
@@ -15,12 +15,17 @@ public class BankAccount {
     }
 
     public void withdraw(double amount) {
+        if (balance == 0) {
+            transactionHistory.add("Withdrawal is denied because the balance is zero.");
+            return;
+        }
         balance -= amount;
         transactionHistory.add("Withdrawal: -" + amount);
     }
 
     public void deposit(double amount) {
-        balance += amount;
-        transactionHistory.add("Deposit: +" + amount);
+        double interest = amount*0.01;
+        balance += amount + interest;
+        transactionHistory.add("Deposit: +" + amount + " (+1% interest)");
     }
 }
